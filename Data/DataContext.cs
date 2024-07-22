6de20aab9f224cb9be14cfdd5a473116eeee1452
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-07-22 21:12:48 +0100

    First Controller

---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
