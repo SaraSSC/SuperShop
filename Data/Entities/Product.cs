dc48108a307f18fc8949e6106e6cac1ea4f3d0f4
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-07-29 16:48:01 +0100

    Add and edit images Images on API Products User on API Products

--- Data/Entities/Product.cs
+++ Data/Entities/Product.cs
@@ -31,5 +31,18 @@ namespace SuperShop.Data.Entities
 
 
         public User User { get; set; }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://localhost:44376{ImageUrl.Substring(1)}";
+            }
+        }
     }
 }
